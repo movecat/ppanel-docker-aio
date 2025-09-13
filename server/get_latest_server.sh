c2d985dbc0127974b983db9b0a73bef556126ac1
movecat <38802164+movecat@users.noreply.github.com>
2025-09-14 00:36:37 +0800

    Update get_latest_server.sh

--- server/get_latest_server.sh
+++ server/get_latest_server.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-LATEST_VER=$(curl -s https://api.github.com/repos/perfect-panel/server/releases/latest | jq -r '.tag_name')
-SERVER_URL="https://github.com/perfect-panel/server/releases/download/${LATEST_VER}/ppanel-server-linux-amd64.tar.gz"
+set -e
+
+SERVER_URL="https://github.com/perfect-panel/server/releases/download/v1.0.5/ppanel-server-linux-amd64.tar.gz"
+
+echo "Downloading fixed version from:"
+echo "${SERVER_URL}"
+
 mkdir -p /opt/ppanel
-wget -O /opt/ppanel-server.tar.gz ${LATEST_VER}
-tar -xvf /opt/ppanel-server.tar.gz -C /opt/ppanel/
+
+# 下载文件
+if curl --head --silent --fail "$SERVER_URL" > /dev/null; then
+    wget -O /opt/ppanel-server.tar.gz "$SERVER_URL"
+    tar -xvf /opt/ppanel-server.tar.gz -C /opt/ppanel/
+else
+    echo "❌ ERROR: URL Not Found: $SERVER_URL"
+    exit 1
+fi
