7be683dec0f708578f63ff35d52173c906a97914
movecat <38802164+movecat@users.noreply.github.com>
2025-09-14 00:32:43 +0800

    Update get_latest_server.sh

--- server/get_latest_server.sh
+++ server/get_latest_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LATEST_VER=$(curl -s https://api.github.com/repos/perfect-panel/server/releases/latest | jq -r '.tag_name')
-SERVER_URL="https://github.com/perfect-panel/server/releases/download/v1.0.5/ppanel-server-linux-amd64.tar.gz"
+SERVER_URL="https://github.com/perfect-panel/server/releases/download/${LATEST_VER}/ppanel-server-linux-amd64.tar.gz"
 mkdir -p /opt/ppanel
-wget -O /opt/ppanel-server.tar.gz https://github.com/perfect-panel/server/releases/download/v1.0.5/ppanel-server-linux-amd64.tar.gz
+wget -O /opt/ppanel-server.tar.gz ${LATEST_VER}
 tar -xvf /opt/ppanel-server.tar.gz -C /opt/ppanel/
